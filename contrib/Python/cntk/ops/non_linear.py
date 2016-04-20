764262cfa0430d40277ce7e44574ff60016dc192
Thilo Will <thilow@microsoft.com>
2016-04-20 16:08:42 +0200

    Renaming python op if_then_else to cond

--- contrib/Python/cntk/ops/non_linear.py
+++ contrib/Python/cntk/ops/non_linear.py
@@ -7,21 +7,17 @@
 
 from cntk.ops.cntk1 import If
 
-def if_then_else(condition, value1, value2, name=None):
+def cond(flag, value_if_true, value_if_false, name=None):
     """
-    Based on first tensor value select one of the two other values. 
-    For the input tensor x, this node outputs a tensor of the same shape with 
-    all of its values clipped to fall between min_value and max_value.
-    The backward pass propagates the received gradient if no clipping occurred,
-    and 0 if the value was clipped.
+    Return either value_if_true or value_if_false based on the value of flag.
+    If flag != 0 value_if_true is returned, otherwise value_if_false.
     
     Args:
-        x: tensor to be clipped
-        condition: the minimum value to clip element values to
-        value1: the maximum value to clip element values to
-        value2: the name of the node in the network            
+        flag: tensor
+        value_if_true: tensor
+        value_if_false: tensor            
     Returns:
         :class:`cntk.graph.ComputationNode`
     """    
     
-    return If(condition, value1, value2, var_name = name)
+    return If(flag, value_if_true, value_if_false, var_name = name)
